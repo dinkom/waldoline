c7dfa519b2714c9d26f3f6f0b6a807f82b70533e
Dinko Mihovilovic <dinko.mih@gmail.com>
2015-05-29 01:53:34 +0200

    Ground units, grid and start button added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Small game for learning basic line slope, built using JavaScript/jQuery
+Small game for learning basic line slope, built using JavaScript/jQuery.
+
+Background image taken from: https://users.cs.jmu.edu/bernstdh/web/common/lectures/slides_coordinate-systems-2d.php
--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+var resetMan = function() {
+	$('#the-man').animate({'left': '3px', 'top': '116px'}, 500);
+};
+var resetField = function() {
+	$('.ground').hide();
+}
+var startGame = function(){
+	$('#the-man').animate({'left': '140px'}, 1000);
+	$('#start').hide();
+};
+
 $(document).ready(function(){
-	$('#the-man').animate({'left': '150px'}, 1000);
+	$('#start').click(startGame);
 });
